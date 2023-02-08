a6951e5d2832de63a68cc28a0f6d773754a53972
student <student@s004-pc14>
2023-02-08 15:16:41 +0100

    Dodaj pliki projektów.

---
+++ aplit-08.02.2023/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -0,0 +1,48 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Console } from 'console';
+
+@Component({
+  selector: 'app-fetch-data',
+  templateUrl: './fetch-data.component.html'
+})
+export class FetchDataComponent implements OnInit {
+  summary:string = ""
+  public summaries: string[] = [];
+  public forecasts: WeatherForecast[] = [];
+
+  constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {
+    http.get<WeatherForecast[]>(baseUrl + 'weatherforecast').subscribe(result => {
+      this.forecasts = result;
+    }, error => console.error(error));
+  }
+  
+  ngOnInit(){
+      this.getSummaries();
+    }
+  getSummaries() {
+    this.http.get<string[]>(this.baseUrl + 'weatherforecast/getSummaries').subscribe(result => {
+      this.summaries = result;
+    }, error => console.error(error));
+  }
+  addSummary() {
+    console.log(this.summary);
+    var summary = {
+      summary: this.summary
+    }
+    this.http.post<string[]>(this.baseUrl + 'weatherforecast/AddSummaries', this.summary).subscribe(result => {
+      this.getSummaries()
+    }, error => console.error(error));
+  }
+
+}
+
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+}
+interface Summary {
+  summary: string;
+}
---
+++ aplit-08.02.2023/Summary.cs
@@ -0,0 +1,7 @@
+﻿namespace aplit_08._02._2023
+{
+    public class Summary
+    {
+        public string SummaryDesc { get; set; }
+    }
+}
